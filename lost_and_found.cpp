824ecd1bd0973845edbbe3773b4227f064a4d5fb
YohanesWisanggeni12 <nicuku7@gmail.com>
2025-09-29 05:33:10 -0700

    lost_and_found.cpp

--- lost_and_found.cpp
+++ lost_and_found.cpp
@@ -2,11 +2,11 @@
 #include <string>
 using namespace std;
 
-// Fungsi user-defined untuk reverse string
+
 string myReverse(string text) {
     int n = text.length();
     for (int i = 0; i < n / 2; i++) {
-        // tukar menggunakan kode ASCII
+       
         int asciiTemp = (int)text[i]; 
         text[i] = text[n - i - 1]; 
         text[n - i - 1] = (char)asciiTemp; 
@@ -19,11 +19,11 @@ int main() {
     cout << "Masukkan kata sandi: ";
     cin >> kata;
 
-    // Enkripsi (dibalik)
+    
     string sandi = myReverse(kata);
     cout << "Hasil enkripsi (dibalik): " << sandi << endl;
 
-    // Dekripsi (dibalik lagi)
+    
     string asli = myReverse(sandi);
     cout << "Kata asli: " << asli << endl;
 
